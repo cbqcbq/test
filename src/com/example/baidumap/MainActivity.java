75b018de02adcf045fe0df413863939660b17a97
cbq <1337715103@qq.com>
2017-03-15 15:46:13 +0800

    First Commit.

---
+++ src/com/example/baidumap/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.baidumap;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends Activity {
+	private MapView mapView;
+	private BaiduMap baidumap;
+	private LocationManager locationManager;
+	private String provider;
+	private boolean isFirstLocate = true;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SDKInitializer.initialize(getApplicationContext());
+		setContentView(R.layout.activity_main);
+		mapView = (MapView) findViewById(R.id.map_view);
+		baidumap = mapView.getMap();
+		baidumap.setMyLocationEnabled(true);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		List<String> providers = locationManager.getProviders(true);
+		if (providers.contains(LocationManager.GPS_PROVIDER))
+			provider = LocationManager.GPS_PROVIDER;
+		else if (providers.contains(LocationManager.NETWORK_PROVIDER))
+			provider = LocationManager.NETWORK_PROVIDER;
+		else {
+			Toast.makeText(this, "no location provider to use",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		Location location = locationManager.getLastKnownLocation(provider);
+		if (location != null)
+			navigateTo(location);
+		locationManager.requestLocationUpdates(provider, 5000, 1,
+				locationListener);
+	}
+
+	LocationListener locationListener = new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onProviderEnabled(String arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onProviderDisabled(String arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onLocationChanged(Location arg0) {
+			if (arg0 != null)
+				navigateTo(arg0);
+		}
+	};
+
+	private void navigateTo(Location location) {
+		if (isFirstLocate) {
+			LatLng ll = new LatLng(location.getLatitude(),
+					location.getLongitude());
+			MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+			baidumap.animateMapStatus(update);
+			MapStatusUpdate update1 = MapStatusUpdateFactory.zoomTo(16f);
+			baidumap.animateMapStatus(update1);
+			isFirstLocate = false;
+		}
+		MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+		locationBuilder.latitude(location.getLatitude());
+		locationBuilder.longitude(location.getLongitude());
+		MyLocationData locationData = locationBuilder.build();
+		baidumap.setMyLocationData(locationData);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		baidumap.setMyLocationEnabled(false);
+		mapView.onDestroy();
+		if (locationManager != null)
+			locationManager.removeUpdates(locationListener);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mapView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+
+}
